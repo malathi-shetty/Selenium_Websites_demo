4f00c9fdf661b6045f556d0c699e83c6c99a92b6
malathi-shetty <shettymalathi113@gmail.com>
2024-08-29 16:47:21 +0530

    Learning

--- Selenium_Websites_demo/src/test/java/excel_Read_Write/Read_Data_From_Excel_1_learn.java
+++ Selenium_Websites_demo/src/test/java/excel_Read_Write/Read_Data_From_Excel_1_learn.java
@@ -13,33 +13,40 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class Read_Data_From_Excel_1_learn {
 
 	public static void main(String[] args) throws IOException {
-		
-		//filepath if inside the package
-		//String excelFilePath =(System.getProperty("user.dir") + "/src/test/java/excel_Read_Write/data.xlsx");
-		
-		//filepath if outside the package
+
+		// filepath if inside the package
+		// String excelFilePath =(System.getProperty("user.dir") +
+		// "/src/test/java/excel_Read_Write/data.xlsx");
+
+		// filepath if outside the package
 		String excelFilePath = "data.xlsx";
 		FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
 		XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
 		XSSFSheet sheet = workbook.getSheet("Sheet1");
-	//String data=sheet.getRow(0).getCell(0).getStringCellValue();
-	//	System.out.println(data);
-		
+
+		/*
+		 * int rows = sheet.getLastRowNum(); // Get the number of rows in the sheet
+		 * System.out.println("Total Rows: " + rows);
+		 * 
+		 * int columns = sheet.getRow(0).getLastCellNum(); // Get the number of columns
+		 * in the first row System.out.println("Total Columns: " + columns);
+		 * 
+		 * String data=sheet.getRow(3).getCell(0).getStringCellValue();
+		 * System.out.println("Row_3 Column_0 Data in Excel Sheet: " + data);
+		 * 
+		 */
 		DataFormatter dataFormatter = new DataFormatter();
 
-        // Iterate through each row
-        for (Row row : sheet) {
-            // Iterate through each cell
-            for (Cell cell : row) {
-                String cellValue = dataFormatter.formatCellValue(cell);
-                System.out.print(cellValue + "\t");
-            }
-            System.out.println(); // Move to the next line
-        }
-
-    } 
-	
+		// Iterate through each row
+		for (Row row : sheet) {
+			// Iterate through each cell
+			for (Cell cell : row) {
+				String cellValue = dataFormatter.formatCellValue(cell);
+				System.out.print(cellValue + "\t");
+			}
+			System.out.println(); // Move to the next line
+		}
 
 	}
 
-
+}
